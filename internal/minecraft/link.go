af3cdaf63e19a27257c1c79c7c3d37c0dcd43da1
Lunidra <keymcc@hotmail.co.uk>
2026-09-19 22:58:24 +0100

    Rework Auth Flow for DEDICATED ETHER LINK SERVER

--- internal/minecraft/link.go
+++ internal/minecraft/link.go
@@ -1,14 +1,6 @@
 package minecraft
 
-import "strings"
-
 type Link struct {
 	UUID     string
 	Username string
-}
-
-func normalizeUUID(uuid string) string {
-	return strings.ToLower(
-		strings.TrimSpace(uuid),
-	)
 }
